c9e0aca3a41dd051ef9dd7a2ccd47f39bd7d61ad
Bao Hoang <88239380+bobbibao@users.noreply.github.com>
2022-01-13 23:00:15 +0700

    Update urls.py

--- poll/urls.py
+++ poll/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views as poll_views
 from .views import Login, Logout
 from django.contrib.auth import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', poll_views.home, name='home'),
     path('create/', poll_views.create, name='create'),
@@ -13,4 +14,4 @@ urlpatterns = [
     path('vote/<poll_id>/', poll_views.vote, name='vote'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(),name='logout'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
